4131fe3691e44084a972871b5d6525f37289d5ee
mdd300 <victor.za.oshiro5@gmail.com>
2018-08-07 17:20:55 -0300

    ajustes

--- application/controllers/Pedido.php
+++ application/controllers/Pedido.php
@@ -128,8 +128,7 @@ class Pedido extends CI_Controller {
         // Instancia a classe mPDF
         $mpdf = new \Mpdf\Mpdf([
             'mode' => 'ut-8',
-            'format' => "A4",
-            'tempDir' => base_url(). '/upload/temp'
+            'format' => "A4"
         ]);
             // Ao invés de imprimir a view 'welcome_message' na tela, passa o código
         // HTML dela para a variável $html
