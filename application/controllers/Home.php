560b052336eba74a54338a08f56122815c47b5ad
mdd300 <victor.za.oshiro5@gmail.com>
2018-08-30 16:28:38 -0300

    ajustes

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -509,6 +509,7 @@ class Home extends CI_Controller {
             $name = $pdf->pdf_nome;
             force_download($name, $data);
         }else{
+            this.$this->visualizarPDF()
         }
     }
 
